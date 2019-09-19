0a3358ce2b3092abfd94878e8ac24b0c0cb7bcad
cuongpc206 <48615102+cuongpc206@users.noreply.github.com>
2019-09-19 11:49:29 +0700

    react-game

---
+++ react-game/precache-manifest.06a8194c7ec36278e6bbf35f91aff415.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8af33bc71bb0483bf8b02e976c51889",
+    "url": "/react-game/index.html"
+  },
+  {
+    "revision": "d6824b23d65995d4e467",
+    "url": "/react-game/static/css/main.560a4157.chunk.css"
+  },
+  {
+    "revision": "b1e9b7aa4894a26fa5ce",
+    "url": "/react-game/static/js/2.3c00251f.chunk.js"
+  },
+  {
+    "revision": "d6824b23d65995d4e467",
+    "url": "/react-game/static/js/main.d4c47841.chunk.js"
+  },
+  {
+    "revision": "f4abe13fae1af131a30f",
+    "url": "/react-game/static/js/runtime~main.067350e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
